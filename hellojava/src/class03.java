8099468b4b8376963adbe85470f9f121bd286f66
fuckingasshole <h0970910231@gmail.com>
2015-08-03 16:00:13 +0800

    v1.0

---
+++ hellojava/src/class03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class class03 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		String name = scn.next();
+		System.out.println("哈囉" +name+ "帥慘了");
+	}
+
+}
